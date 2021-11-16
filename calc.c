c936c07c9131af3caf4e03c40e7fd5852268e955
Milo4uk <wlir0tn@gmail.com>
2021-11-16 19:07:20 +0300

    new

--- calc.c
+++ calc.c
@@ -6,7 +6,6 @@
 //
 
 #include <stdio.h>
-#include <iostream>
 #include <string.h>
 
 char Num;
@@ -18,35 +17,46 @@ void input(N, B1, B2) {
     printf("Write your number:\n");
     scanf_s("%s", &N);
     printf("Write the base:\n");
-    scanf_s("%d", &B1);
+    scanf_s()("%d", &B1);
     printf("Write the new base:\n (2,)");
-    scanf_s("%d", &B2);
+    scanf_s()("%d", &B2);
 }
 
 int convertation_dec() {
-
+    return 0;
 }
 
 char convaertation_newBase() {
     //приходит int потом делаем %Base2 и после записываем это как стринг
     char p;
-    int remainder,i ,j =0;
+    int remainder, i = 0, l;
     char M[i];
+    int Cont[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+    
+    p = convertation_dec(Numb, Base1);
+    remainder = p;
     
     if (Base2<10){
-        while ((remainder/Base2)>Base1) {
+        do {
             p = (remainder%Base2);
             scanf("%s", &M[i++]); // записываем в массив
             remainder = (p/Base2);
+        } while ((remainder/Base2)>Base1);
+    
+    } else do {
+        if (Base2>10 && Base2<=16){
+        if (p = (remainder%Base2)>9) {
+            p = (remainder%Base2);
+            l = p % 10;
+            p = Cont[l];
+            //сделать * на место в массиве как варик...
         }
-    
-    } else if (Base2>10){
-        
+        scanf("%s", &M[i++]);
+        remainder = (p/Base2);
     }
+    } while ((remainder/Base2)>Base1);
     
-    p = strrev(M[i]);
-        
-    }
+    p = strrev(M);
     return p;
-}
+    }
 
