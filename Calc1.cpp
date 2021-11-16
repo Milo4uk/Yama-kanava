1e746de84d09d104f0997f9982df97a6777c562b
Ishtar624 <92232270+Ishtar624@users.noreply.github.com>
2021-11-16 15:40:04 +0300

    Add files via upload

---
+++ Calc1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+
+char Num;
+int B1, B2;
+char Numb[16] = { '0','1','2','3','4','5','6','7','8','9','A', 'B', 'C', 'D', 'E', 'F'};
+
+
+void input(N, B1, B2) {
+	printf("Write your number:\n");
+	scanf_s("%d", &N);
+	printf("Write the base:\n");
+	scanf_s("%d", &B1);
+	printf("Write the new base:\n");
+	scanf_s("%d", &B2);
+}
+
+char convertation_dec() {
+
+}
+
+int main() {
+	input(Num);
+
+	return 0;
+}
